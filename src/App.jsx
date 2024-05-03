1ea2cbedbe8a0ad5af4f6f8b14719f5c835bff62
Franco Baldomar <franco19999@gmail.com>
2024-05-03 11:00:52 -0300

    ecommerce baldomar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { NavBar } from "./components/NavBar";
 import { ItemListContainer } from "./components/ItemListContainer";
-
+import { ItemDetailContainer } from "./components/ItemDetailContainer";
 function App() {
   return (
     <>
@@ -11,6 +11,8 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<ItemListContainer />}></Route>
+          <Route path="/category/:id" element={<ItemListContainer />}></Route>
+          <Route path="/item/:id" element={<ItemDetailContainer />}></Route>
         </Routes>
       </BrowserRouter>
     </>
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,6 +1,5 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-
 export const item = ({ item }) => (
   <Card style={{ width: "18rem" }}>
     <Card.Img variant="top" src={item.Img} />
@@ -8,7 +7,7 @@ export const item = ({ item }) => (
       <Card.Title>{item.name}</Card.Title>
       <Card.Text>{item.detail}</Card.Text>
       <Card.Text>{item.category}</Card.Text>
-      <link to={"/item/${item.id}"}>
+      <link to={`/item/${item.id}`}>
         <Button variant="primary">Go somewhere</Button>
       </link>
     </Card.Body>
